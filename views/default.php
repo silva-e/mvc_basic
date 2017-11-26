787639793a7fe98976f66c260c3838e50c5ab7fe
Élisson silva <suporte@efdinformatica.com.br>
2017-11-26 15:07:31 -0200

    Adicionado pasta de assets com bootstrap 3.3.7 e jquery 3.1

--- views/default.php
+++ views/default.php
@@ -3,10 +3,16 @@
 <head>
 	<meta charset="UTF-8">
 	<title>	<?=$title ?></title>
+	<link rel="stylesheet" type="text/css" href="<?=BASE_URL.'/assets/bootstrap/bootstrap-3.3.7-dist/css/bootstrap.min.css'?>">
 </head>
 <body>
 <?php var_dump($dados); ?>
 
+	
+	<script type="text/javascript" src="<?=BASE_URL.'/assets/js/jquery-3.2.1.min.js'?>"></script>
 
+	<script type="text/javascript" src="<?=BASE_URL.'/assets/js/myApp.js'?>"></script>
+
+	<script type="text/javascript" src="<?=BASE_URL.'/assets/bootstrap/bootstrap-3.3.7-dist/js/bootstrap.min.js'?>"></script>
 </body>
 </html>
